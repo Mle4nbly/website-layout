0ff398e8bbaa21cd8cc50dec0eb64b387685e9d2
mle4nbly <mle4nbly@gmail.com>
2026-04-19 23:13:09 +0700

    Nuxt cfg change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css']
+
+  css: ['~/assets/css/main.css'],
+
+  app: {
+    baseURL: '/website-layout/'
+  },
+
+  ssr: false,
+
+  nitro: {
+    preset: 'static'
+  }
 })
